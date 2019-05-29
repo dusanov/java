d7c7d5fad558f551d5db268c6a0d50e8a8a1dfdb
dusanov <hrandus@yahoo.com>
2019-05-29 13:59:30 +0200

    cleanup

---
+++ temp/src/TestRTE.java
@@ -0,0 +1,14 @@
+class TestRTE{
+
+    public static void main(String ... args){
+   /* 
+        int[][] arr = new int[1][]; 
+        arr[0][0] = 2;
+    */
+        String str = null;
+        if (str == null){System.out.println(" it's null");}
+        //else (str.length() == 0){System.out.println("it's size zero");}
+        else {System.out.println(" else");}
+    }
+
+}
